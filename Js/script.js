327692fc5d87d4faa3b37dd417b9fa158fac236d
myslikam <ammyslik@gmail.com>
2020-12-25 16:57:23 +0300

    ferst

---
+++ Js/script.js
@@ -0,0 +1,80 @@
+/* 'use strict'; */
+
+/* let number = 5;
+const MuyNumber = 25;
+number = 25;
+console.log(number);
+
+number = MuyNumber - 25;
+console.log(number);
+ 
+const age = {
+    name: "Sasha",
+    higHt: 176,
+    family: "myslik"
+};
+
+console.log(age.higHt);
+
+const arr = ["barsik", "tosha"];
+
+console.log(arr[1]); */
+
+/* alert("Hello"); */
+
+/* const newChips = confirm("Будешь чипсы");
+console.log(newChips); */
+/* 
+let ear = +prompt('Сколько тебе лет', 'напиши');
+console.log(typeof(ear)); */
+
+/* const arr = [];
+
+/* arr[0] = prompt('Имя','');
+arr[1] = prompt('Фамилия','');
+arr[2] = prompt('Год рождения',''); 
+/* document.write(arr); */
+
+/* console.log(typeof(arr));  */
+
+/* let collInfo = [];
+
+collInfo[0] = +prompt('Расход сараем','Гкал');
+collInfo[1] = +prompt('Расход сараем2','Гкал');
+
+let summa = collInfo[0] + collInfo[1];
+
+document.write(`потребление ${summa} Гкал`); */
+
+/* let nameLoh = prompt('Введите имя','имя');
+alert(`Привет ${nameLoh}`); */
+
+/* let a = +prompt('Введите вес','');
+let b = +prompt('Введите толщину','');
+let c = a + b;
+document.write(`Вы получили ${c} кг метров`);
+ */
+
+/*  let incr = 10,
+     decr = 10; */
+
+/*  ++incr;
+ --decr;
+ console.log(incr);
+ console.log(decr); */
+
+ /* --incr;
+ ++decr; */
+/*  console.log(incr++);
+ console.log(decr--);
+ console.log(incr);
+ console.log(decr); */
+
+/*  console.log(11%4); */
+
+/* console.log(5*2 === '10'); */
+
+/* const a = true,
+      b = false; */
+    
+console.log(5 !== '5');
